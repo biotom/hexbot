02818967303ff3cf9195945bdf682f0431451698
Tom Deadman <tom.deadman@river-island.com>
2019-08-14 18:29:44 +0100

    http server added, test  service structs  added

--- internal/service/service_test.go
+++ internal/service/service_test.go
@@ -2,34 +2,82 @@ package service_test
 
 import (
 	"errors"
+	"fmt"
+	"github.com/River-Island/product-backbone-v2/logging"
+	"github.com/golang/mock/gomock"
+	"hexbot/internal/service"
+	"io/ioutil"
 	"net/http"
 	"net/http/httptest"
 	"testing"
 )
 
+
+type testService struct {
+	ctrl *gomock.Controller
+	log *logging.Logger
+	mockDB *service.MockDatabase
+	service	*service.ColourService
+}
+
+func newTestService(t *testing.T) *testService {
+	ctrl := gomock.NewController(t)
+	db := service.NewMockDatabase(ctrl)
+	s := &testService{
+		ctrl:    ctrl,
+		mockDB:   db,
+		service: service.NewColourService(logging.NopLogger, ),
+		//needs a hexbot client
+	}
+	return s
+}
+
 func TestColourService_FetchColourFromHexbot(t *testing.T) {
 
-	tests  := []struct{
-		Desc string
-		ExpectedError error
+	tests := []struct {
+		Desc            string
+		ExpectedError   error
 		StartTestServer bool
 	}{
 		{
-			Desc: "fails due to GET error",
-			ExpectedError: errors.New("problem getting hex from hexbot"),
+			Desc:            "fails due to GET error",
+			ExpectedError:   errors.New("problem getting hex from hexbot"),
 			StartTestServer: true,
-
 		},
 	}
 
-	for tt := range tests {
+	for _, tt := range tests {
 		t.Run(tt.Desc, func(t *testing.T) {
+			if tt.StartTestServer == true {
 
-			if StartTestServer == true {
-				server := httptest.NewServer()
+				ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+					_, err  := fmt.Fprintln(w, "#228B22")
+					if err != nil {
+						t.Fatal(err)
+					}
+				}))
+				defer ts.Close()
 
-		})
 
-	}
+				//this happens inside the colour service
+				resp, err := http.Get(ts.URL)
+				if err != nil {
+					t.Fatal(err)
+				}
+
+
+				colour, err := ioutil.ReadAll(resp.Body)
+				resp.Body.Close()
+				if err != nil{
+					t.Fatal(err)
+				}
+
+
+
+		}
+
+	})
+
+}
 
 }
